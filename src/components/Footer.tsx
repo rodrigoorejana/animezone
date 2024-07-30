736611d2ef758777ef1b48100510d4725452a82b
Rodrigo O Costa <rodrigoorejana.rc@gmail.com>
2024-07-30 17:07:35 -0300

    adição do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,12 @@ const Footer = () => {
       <div className='container-fluid text-center '>
       <p>© 2024 Rodrigo Orejana da Costa. All rights reserved.</p>
 
-        <div className="socialLinks">
+        <div className="socialLinks row">
           <a href="https://www.linkedin.com/in/rodrigo-orejana-da-costa" target="_blank" rel="noopener noreferrer">
           <FaLinkedin size={24} /> rodrigo-orejana-da-costa
           </a>
           <a href="https://github.com/rodrigoorejana/animezone" target="_blank" rel="noopener noreferrer">
-          <FaGithub size={24} /> rodrigoorejana
+           <FaGithub size={24} /> rodrigoorejana
           </a>
         </div>
       </div>
